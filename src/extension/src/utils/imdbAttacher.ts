41f64d51b774c8b604bb306622507870fea15c80
Vlad Jerca <vlad.jerca@gmail.com>
2024-09-18 13:55:07 +0200

    feat(series): attach imdb id's to individual episodes

--- src/extension/src/utils/imdbAttacher.ts
+++ src/extension/src/utils/imdbAttacher.ts
@@ -4,8 +4,23 @@ import { imdb } from '../request/topic/imdb';
 
 export async function imdbAttacher(list: Array<Movie | Series>, type: 'movie' | 'series') {
     for (const media of list) {
+        if (type === 'series') {
+            for (const season of (media as Series).seasons) {
+                for (const episode of season.episodes) {
+                    episode.id.imdb = await imdb({
+                        episodeId: episode.id.tvdb,
+                        showId: media.id.tvdb,
+                        type: 'episode'
+                    });
+                }
+            }
+        }
+
         if (media.id.imdb === '-1') {
-            media.id.imdb = await imdb(media.id.tvdb, type);
+            media.id.imdb = await imdb({
+                type,
+                id: media.id.tvdb
+            });
 
             if (media.id.imdb === '-1') {
                 console.log(`Failed to find IMDB ID for ${media.title}!`);
