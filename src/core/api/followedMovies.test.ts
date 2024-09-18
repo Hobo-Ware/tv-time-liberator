c8803e0766d788d99c02fbc575a7ba3b4fa1d60b
Vlad Jerca <vlad.jerca@gmail.com>
2024-09-18 20:49:20 +0200

    test(favorites): define favorite integration

--- src/core/api/followedMovies.test.ts
+++ src/core/api/followedMovies.test.ts
@@ -3,30 +3,7 @@ import { login } from '../../cli/login';
 import { assertDefined } from '../utils/assertDefined';
 import { setAuthorizationHeader } from '../http/setAuthorizationHeader';
 import { followedMovies } from './followedMovies';
-
-const mr_nobody_watched = {
-    id: {
-        tvdb: 1571,
-        imdb: "-1",
-    },
-    created_at: "2024-09-13T10:49:37Z",
-    uuid: "409c29e8-8daf-41bd-843a-8a217320d374",
-    title: "Mr. Nobody",
-    watched_at: "2024-09-13T10:49:37Z",
-    is_watched: true,
-};
-
-const the_matrix_not_watched = {
-    id: {
-        tvdb: 169,
-        imdb: "-1",
-    },
-    created_at: "2024-09-13T10:49:58Z",
-    uuid: "978899c4-5194-4568-b922-0bd2874c4c1a",
-    title: "The Matrix",
-    watched_at: undefined,
-    is_watched: false,
-}
+import { mr_nobody_watched, the_matrix_not_watched } from '../../.test/data';
 
 describe.only('followedMovies', () => {
     it('should fetch followed movies', async () => {
