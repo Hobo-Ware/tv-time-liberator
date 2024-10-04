ee6e4f92d665f3cbcd9c00b5f2b5466358c0dfae
Vlad Jerca <vlad.jerca@gmail.com>
2024-10-04 17:00:32 +0200

    perf(cli): keep in-memory copy of db for cli extraction

--- test/db.mock.ts
+++ test/db.mock.ts
@@ -1,29 +1,10 @@
 import { mock } from 'bun:test';
-
-class FSDBMock {
-    private memory = new Map();
-
-    async get(key: string) {
-        return this.memory.get(key);
-    }
-
-    async set(key: string, value: any) {
-        this.memory.set(key, value);
-    }
-
-    async has(key: string) {
-        return this.memory.has(key);
-    }
-
-    async delete(key: string) {
-        this.memory.delete(key);
-    }
-}
+import { MemoryDB } from '../src/cli/store/MemoryDB';
 
 mock.module('file-system-db', () => {
     console.log('--- Mocking file-system-db ---');
 
     return {
-        FSDB: FSDBMock,
+        FSDB: function () { return new MemoryDB({}) },
     };
 });
