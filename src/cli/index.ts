5ed433905a5eb5ddc40453bb0bc41540ca7c0ff7
Vlad Jerca <vlad.jerca@gmail.com>
2024-10-05 14:43:28 +0200

    feat(cli): export as flattened csv

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,6 +5,7 @@ import { setAuthorizationHeader } from '../core/http/setAuthorizationHeader';
 import { setCache } from '../core/http';
 import { PersistentStore } from './store';
 import progress from 'cli-progress';
+import { toCsv } from '../core/serializer/toCsv';
 
 const reporter = new progress.SingleBar({
     format: '{bar} {percentage}% | {category} \t| ETA: {estimated}s | {message} ',
@@ -57,9 +58,21 @@ await writeFile('.export/movies.json', JSON.stringify(movies, null, 4))
 
 const shows = await followedShows(configFactory('Series'));
 await writeFile('.export/shows.json', JSON.stringify(shows, null, 4));
+await writeFile('.export/watchlist.csv', toCsv({
+    movies,
+    shows,
+}));
 
 const favorites = await favoriteList(configFactory('Faves'));
 await writeFile('.export/favorites.json', JSON.stringify(favorites, null, 2));
+await writeFile('.export/favorites.csv', toCsv(favorites));
 
 const lists = await myLists(configFactory('Lists'));
 await writeFile('.export/lists.json', JSON.stringify(lists, null, 2));
+for (const list of lists) {
+    const listFilePath = `.export/list_${list.name.toLowerCase().replace(/ /g, '_')}.csv`;
+    await writeFile(listFilePath, toCsv({
+        movies: list.movies,
+        shows: list.shows,
+    }));
+}
