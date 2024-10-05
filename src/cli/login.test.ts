c9fc09f6380d484a2c54bf5a79728d6698e40512
seferturan <seferturan85@gmail.com>
2024-10-05 14:16:17 +0200

    test: remove only from some describes

--- src/cli/login.test.ts
+++ src/cli/login.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'bun:test';
 import { login } from './login';
 import { assertDefined } from '../core/utils/assertDefined';
 
-describe.only('login', () => {
+describe('login', () => {
     it('should fetch token', async () => {
         const username = assertDefined(process.env.TV_TIME_TEST_USERNAME, 'TV_TIME_TEST_USERNAME not defined.');
         const password = assertDefined(process.env.TV_TIME_TEST_PASSWORD, 'TV_TIME_TEST_PASSWORD not defined.');
