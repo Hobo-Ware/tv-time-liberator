f6c6de205e9e995d01ee7b903c27bb37440b3691
Vlad Jerca <vlad.jerca@gmail.com>
2024-10-07 13:52:42 +0200

    perf: skip html parsing step, iMDB can be extracted directly

--- src/core/api/toIMDB.ts
+++ src/core/api/toIMDB.ts
@@ -1,22 +1,8 @@
-import type { HTMLElement } from 'node-html-parser';
 import { assertDefined } from '../utils/assertDefined';
 import type { IMDBReference, IMDBUndefined } from '../types/IMDBReference';
 import { request } from '../http';
 import { LIBERATE_IMDB_MOVIE } from '../../cli/env/LIBERATE_IMDB_MOVIE';
 import { LIBERATE_IMDB_SHOW } from '../../cli/env/LIBERATE_IMDB_SHOW';
-
-async function parseHtml(html: string): Promise<HTMLElement> {
-    if (globalThis.DOMParser) {
-        const parser = new DOMParser();
-        const document = parser.parseFromString(html, 'text/html');
-        return document.body as unknown as HTMLElement;
-    }
-
-    return import('node-html-parser')
-        .then(({ parse }) => {
-            return parse(html) as HTMLElement;
-        });
-}
 
 export type DereferrerOptions = {
     id: number;
@@ -60,15 +46,10 @@ export async function toIMDB(options: DereferrerOptions): Promise<IMDBReference>
         },
         responseType: 'text',
     })
-        .then(markup => parseHtml(markup))
         .then(doc => {
-            const imdbRef = assertDefined(doc.querySelector('a[href^="https://www.imdb.com/title/"]'), 'IMDB reference not found.');
-
             const regex = /https:\/\/www\.imdb\.com\/title\/(tt\d+)\//;
-            const [, imdb] = imdbRef.getAttribute('href')?.match(regex) || [];
+            const [, imdb] = doc.match(regex) || [];
             return assertDefined(imdb as IMDBReference, `IMDB ID not found for ${JSON.stringify(options)}.`);
         })
         .catch(() => '-1' as IMDBUndefined);
 }
-
-
