8e05a4f3cd5c81b431cb703b6c7a13063327eb2c
Vlad Jerca <vlad.jerca@gmail.com>
2024-11-20 14:44:29 +0100

    refactor: switch to @jsr/csv to enable browser

--- src/core/serializer/toCsv.test.ts
+++ src/core/serializer/toCsv.test.ts
@@ -1,48 +1,87 @@
-import { describe, expect, it } from 'bun:test';
-import { toCsv } from './toCsv';
-import { mr_nobody_watched, the_matrix_not_watched, chernobyl_up_to_date, house_usher_continuing } from '../../.test/data';
+import { describe, expect, it } from "bun:test";
+import { toCsv } from "./toCsv";
+import {
+    chernobyl_up_to_date,
+    house_usher_continuing,
+    mr_nobody_watched,
+    the_matrix_not_watched,
+} from "../../.test/data";
 
-describe('toCsv', () => {
-
-    it('should serialize movies', () => {
+describe("toCsv", () => {
+    it("should serialize movies", () => {
         const result = toCsv({
             movies: [mr_nobody_watched, the_matrix_not_watched] as any,
             shows: [],
         });
 
         expect(result).toBeString();
-        const [header, nobody, matrix] = result.split('\n');
-        expect(header).toBe('"id_imdb","id_tvdb","type","title","season","episode","is_special","is_watched","watched_at","status"');
-        expect(nobody).toBe(`"${mr_nobody_watched.id?.imdb}",${mr_nobody_watched.id?.tvdb},"movie","${mr_nobody_watched.title}","","",0,${Number(mr_nobody_watched.is_watched)},"${mr_nobody_watched.watched_at}",""`);
-        expect(matrix).toBe(`"${the_matrix_not_watched.id?.imdb}",${the_matrix_not_watched.id?.tvdb},"movie","${the_matrix_not_watched.title}","","",0,${Number(the_matrix_not_watched.is_watched)},"",""`);
+        const [header, nobody, matrix] = result.split("\n");
+        console.log(header);
+        expect(header).toBe(
+            "id_imdb,id_tvdb,type,title,season,episode,is_special,is_watched,watched_at,status\r",
+        );
+        expect(nobody).toBe(
+            `${mr_nobody_watched.id?.imdb},${mr_nobody_watched.id?.tvdb},movie,${mr_nobody_watched.title},,,false,${mr_nobody_watched.is_watched},${mr_nobody_watched.watched_at},\r`,
+        );
+        expect(matrix).toBe(
+            `${the_matrix_not_watched.id?.imdb},${the_matrix_not_watched.id?.tvdb},movie,${the_matrix_not_watched.title},,,false,${the_matrix_not_watched.is_watched},,\r`,
+        );
     });
 
-    it('should serialize shows', () => {
+    it("should serialize shows", () => {
         const result = toCsv({
             movies: [],
             shows: [chernobyl_up_to_date, house_usher_continuing] as any,
         });
 
         expect(result).toBeString();
-        const [header, ...rest] = result.split('\n');
-        expect(header).toBe('"id_imdb","id_tvdb","type","title","season","episode","is_special","is_watched","watched_at","status"');
+        const [header, ...rest] = result.split("\n");
+        expect(header).toBe(
+            "id_imdb,id_tvdb,type,title,season,episode,is_special,is_watched,watched_at,status\r",
+        );
 
         chernobyl_up_to_date.seasons
             .forEach(({ number: season, episodes }) => {
                 episodes
-                    .forEach(({ id, special, number: episode, is_watched, watched_at }) => {
-                        const row = rest.shift();
-                        expect(row).toBe(`"${id?.imdb}",${id?.tvdb},"show","${chernobyl_up_to_date.title}",${season},${episode},${Number(special)},${Number(is_watched)},"${watched_at}","${chernobyl_up_to_date.status}"`);
-                    });
+                    .forEach(
+                        (
+                            {
+                                id,
+                                special,
+                                number: episode,
+                                is_watched,
+                                watched_at,
+                            },
+                        ) => {
+                            const row = rest.shift();
+                            expect(row).toBe(
+                                `${id?.imdb},${id?.tvdb},show,${chernobyl_up_to_date.title},${season},${episode},${special},${is_watched},${watched_at},${chernobyl_up_to_date.status}\r`,
+                            );
+                        },
+                    );
             });
 
         house_usher_continuing.seasons
             .forEach(({ number: season, episodes }) => {
                 episodes
-                    .forEach(({ id, special, number: episode, is_watched, watched_at }) => {
-                        const row = rest.shift();
-                        expect(row).toBe(`"${id?.imdb}",${id?.tvdb},"show","${house_usher_continuing.title}",${season},${episode},${Number(special)},${Number(is_watched)},"${watched_at ?? ''}","${house_usher_continuing.status}"`);
-                    });
+                    .forEach(
+                        (
+                            {
+                                id,
+                                special,
+                                number: episode,
+                                is_watched,
+                                watched_at,
+                            },
+                        ) => {
+                            const row = rest.shift();
+                            expect(row).toBe(
+                                `${id?.imdb},${id?.tvdb},show,${house_usher_continuing.title},${season},${episode},${special},${is_watched},${
+                                    watched_at ?? ""
+                                },${house_usher_continuing.status}\r`,
+                            );
+                        },
+                    );
             });
     });
 });
--- src/core/serializer/toCsv.ts
+++ src/core/serializer/toCsv.ts
@@ -1,24 +1,24 @@
-import { Movie } from '../types/Movie';
-import { Show } from '../types/Show';
-import { stringify } from 'csv-stringify/sync';
+import { Movie } from "../types/Movie";
+import { Show } from "../types/Show";
+import { stringify } from "@std/csv";
 
 const header = [
-    'id_imdb',
-    'id_tvdb',
-    'type',
-    'title',
-    'season',
-    'episode',
-    'is_special',
-    'is_watched',
-    'watched_at',
+    "id_imdb",
+    "id_tvdb",
+    "type",
+    "title",
+    "season",
+    "episode",
+    "is_special",
+    "is_watched",
+    "watched_at",
     "status",
 ];
 
 type CsvParams = {
     shows: Partial<Show>[];
     movies: Partial<Movie>[];
-}
+};
 export function toCsv({
     shows,
     movies,
@@ -28,19 +28,19 @@ export function toCsv({
             id,
             title,
             is_watched,
-            watched_at
+            watched_at,
         }) => [
-                id?.imdb,
-                id?.tvdb,
-                'movie',
-                title,
-                '',
-                '',
-                false,
-                is_watched,
-                watched_at,
-                '',
-            ]);
+            id?.imdb,
+            id?.tvdb,
+            "movie",
+            title,
+            "",
+            "",
+            false,
+            is_watched,
+            watched_at,
+            "",
+        ]);
 
     const showCsvEntries = shows
         .flatMap(({
@@ -48,41 +48,35 @@ export function toCsv({
             seasons,
             status,
         }) =>
-            seasons
-                ?.flatMap(({
-                    number: season,
-                    episodes,
-                }) =>
-                    episodes
-                        .map(({
-                            id,
-                            special,
-                            number: episode,
-                            is_watched,
-                            watched_at,
-                        }) => [
-                                id?.imdb,
-                                id?.tvdb,
-                                'show',
-                                title,
-                                season,
-                                episode,
-                                special,
-                                is_watched,
-                                watched_at,
-                                status,
-                            ])
-                ));
+            (seasons ?? []).flatMap(({
+                number: season,
+                episodes,
+            }) =>
+                episodes
+                    .map(({
+                        id,
+                        special,
+                        number: episode,
+                        is_watched,
+                        watched_at,
+                    }) => [
+                        id?.imdb,
+                        id?.tvdb,
+                        "show",
+                        title,
+                        season,
+                        episode,
+                        special,
+                        is_watched,
+                        watched_at,
+                        status,
+                    ])
+            )
+        );
 
     return stringify([
         header,
         ...movieCsvEntries,
         ...showCsvEntries,
-    ], {
-        quoted_empty: true,
-        quoted_string: true,
-        cast: {
-            boolean: (value) => value ? '1' : '0',
-        }
-    });
-} 
+    ]);
+}
