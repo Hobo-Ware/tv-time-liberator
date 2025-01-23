5c1d25b3f58cfe6ab492849ab8e89acffa1b18fd
Vlad Jerca <vlad.jerca@gmail.com>
2025-01-23 14:20:34 +0100

    fix: launch puppeteer without sandbox

--- src/cli/token.ts
+++ src/cli/token.ts
@@ -1,24 +1,27 @@
-import puppeteer from 'puppeteer';
-import { Resource } from '../core/http/Resource';
-import { assertDefined } from '../core/utils/assertDefined';
+import { launch } from "puppeteer";
+import { Resource } from "../core/http/Resource";
+import { assertDefined } from "../core/utils/assertDefined";
 
 export async function getToken() {
-    const browser = await puppeteer.launch();
+    const browser = await launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
-    console.log('Navigating to TV Time...');
+    console.log("Navigating to TV Time...");
     await page.goto(Resource.Browser.Homepage);
 
-    console.log('Waiting for token...');
+    console.log("Waiting for token...");
     await page.waitForFunction(() => {
-        return (localStorage as Storage).getItem('flutter.jwtToken') != null;
+        return (localStorage as Storage).getItem("flutter.jwtToken") != null;
     });
 
-    console.log('Token found...');
+    // skipcq: JS-0002
+    console.log("Token found...");
     const localStorage = await page.evaluate(() => {
         const ls = localStorage as Storage;
         const json: Record<string, string | null> = {};
         for (let i = 0; i < ls.length; i++) {
-            const key = ls.key(i) ?? 'unknown';
+            const key = ls.key(i) ?? "unknown";
             json[key] = ls.getItem(key);
         }
         return json;
@@ -26,5 +29,8 @@ export async function getToken() {
 
     await browser.close();
 
-    return assertDefined(localStorage['flutter.jwtToken'], 'Flutter token not found.').slice(1, -1);
+    return assertDefined(
+        localStorage["flutter.jwtToken"],
+        "Flutter token not found.",
+    ).slice(1, -1);
 }
